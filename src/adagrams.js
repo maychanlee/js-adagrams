1b693e5609925b40a0f54d2816a67ae3e4def072
May Lee <mayhclee@gmail.com>
2025-12-01 20:11:13 -0800

    Created helper function and drawletter function; finshed wave 1

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,37 @@
+// Variables for Project
+const LETTER_POOL = {
+  'A': 9, 'B': 2, 'C': 2, 'D': 4, 'E': 12, 'F': 2,
+  'G': 3, 'H': 2, 'I': 9, 'J': 1, 'K': 1, 'L': 4,
+  'M': 2, 'N': 6, 'O': 8, 'P': 2, 'Q': 1, 'R': 6,
+  'S': 4, 'T': 6, 'U': 4, 'V': 2, 'W': 2, 'X': 1,
+  'Y': 2, 'Z': 1,
+};
+
+//helper function to get convert our letter pool to a list
+const drawableAvailableLetters = (letterPool) => {
+  let availableLetters = [];
+  for (const [letter, qty] of Object.entries(letterPool)) {
+    for (let i = 0; i < qty; i++) {
+      availableLetters.push(letter);
+    };
+  };
+  return availableLetters;
+};
+
 export const drawLetters = () => {
   // Implement this method for wave 1
+  const letterPool = drawableAvailableLetters(LETTER_POOL);
+  let outputLetters = [];
+
+  const MAX_HAND_LENGTH = 10;
+
+  while (outputLetters.length < MAX_HAND_LENGTH) {
+    const index = Math.floor(Math.random() * letterPool.length);
+    outputLetters.push(letterPool[index]);
+    [letterPool[index], letterPool[letterPool.length - 1]] = [letterPool[letterPool.length - 1], letterPool[index]];
+    letterPool.pop();
+  }
+  return outputLetters;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
